26c36dd48bb1f20ba49a62dedd2264d54d3afac6
Vhuy12 <vuonghuy9660@gmail.com>
2025-07-02 17:05:46 +0700

    Update Vampiretest.lua

--- Vampiretest.lua
+++ Vampiretest.lua
@@ -2333,48 +2333,65 @@ VisualFilterSection:AddSlider({
 	end
 })
 
-local fpsBoostEnabled = false
+local fpsSmoothEnabled = false
 
-local fpsSection = world:AddSection({
-	Name = "FPS Booster",
+local fpsSmoothSection = world:AddSection({
+	Name = "FPS Booster Smooth",
 	Position = "left"
 })
 
-fpsSection:AddToggle({
-	Name = "Enable FPS Booster",
+fpsSmoothSection:AddToggle({
+	Name = "Enable Smooth Mode",
 	Callback = function(state)
-		fpsBoostEnabled = state
+		fpsSmoothEnabled = state
 		if state then
-			enableFPSBoost()
+			enableSmoothMode()
 		else
-			disableFPSBoost()
+			disableSmoothMode()
 		end
 	end
 })
 
-pcall(function()
-	settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
-end)
+function enableSmoothMode()
+	pcall(function()
+		settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
+	end)
 
-local lighting = game:GetService("Lighting")
-lighting.GlobalShadows = false
-lighting.Brightness = 1
-lighting.ClockTime = 13.5
-lighting.FogEnd = 1e9
-lighting.ExposureCompensation = 0.1
+	local lighting = game:GetService("Lighting")
+	lighting.GlobalShadows = false
+	lighting.Brightness = 2
+	lighting.ClockTime = 13.5
+	lighting.FogEnd = 1e9
+	lighting.ExposureCompensation = 0.5
 
-for _, obj in ipairs(game:GetDescendants()) do
-	if obj:IsA("BasePart") then
-		obj.Material = Enum.Material.SmoothPlastic
-		obj.Reflectance = 0
-		obj.Color = Color3.fromRGB(60, 60, 60) -- sáng hơn
-	elseif obj:IsA("Texture") or obj:IsA("Decal") then
-		obj:Destroy()
+	for _, obj in ipairs(game:GetDescendants()) do
+		if obj:IsA("BasePart") then
+			obj.Material = Enum.Material.SmoothPlastic
+			obj.Reflectance = 0
+			obj.Color = Color3.fromRGB(70, 70, 70)
+		elseif obj:IsA("Texture") or obj:IsA("Decal") then
+			obj:Destroy()
+		end
+	end
+
+	for _, fx in ipairs(lighting:GetDescendants()) do
+		if fx:IsA("BlurEffect") or fx:IsA("SunRaysEffect") or fx:IsA("BloomEffect") or fx:IsA("ColorCorrectionEffect") then
+			fx.Enabled = false
+		end
 	end
 end
 
-for _, fx in ipairs(lighting:GetDescendants()) do
-	if fx:IsA("BlurEffect") or fx:IsA("SunRaysEffect") or fx:IsA("BloomEffect") or fx:IsA("ColorCorrectionEffect") then
-		fx.Enabled = false
+function disableSmoothMode()
+	local lighting = game:GetService("Lighting")
+	lighting.GlobalShadows = true
+	lighting.Brightness = 2
+	lighting.ClockTime = 14
+	lighting.FogEnd = 1000
+	lighting.ExposureCompensation = 0.3
+
+	for _, fx in ipairs(lighting:GetDescendants()) do
+		if fx:IsA("BlurEffect") or fx:IsA("SunRaysEffect") or fx:IsA("BloomEffect") or fx:IsA("ColorCorrectionEffect") then
+			fx.Enabled = true
+		end
 	end
-end
+						end
